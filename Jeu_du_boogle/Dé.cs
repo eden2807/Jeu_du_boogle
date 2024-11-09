73c280aa04714640b922b936f844722d9061dccf
delfassy <elfdav@yahoo.fr>
2024-11-09 22:03:31 +0100

    Ajour de la classe "Dé"

---
+++ Jeu_du_boogle/Dé.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_du_boogle
+{
+    public class Dé
+    {
+
+    }
+}
