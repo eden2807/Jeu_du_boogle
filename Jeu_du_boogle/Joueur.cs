64d0b0756ffd550382bde35876da881db8ea999f
Ryan Fihr HULTON <ryan-fihr.hulton@edu.devinci.fr>
2024-11-09 23:43:05 +0100

    Classe Joueur : le design de base a été ajouté

--- Jeu_du_boogle/Joueur.cs
+++ Jeu_du_boogle/Joueur.cs
@@ -8,6 +8,46 @@ namespace Jeu_du_boogle
 {
     public class Joueur
     {
+        //-----[ Attributs ]-----//
+        #region Attributs
+
         private string nom;
+        private int score;
+        private int nbMotsTrouves;
+
+        #endregion
+
+
+        //---[ Constructeurs ]---//
+        #region Constructeurs
+
+        // Constructeur naturel
+        public Joueur() { }
+
+        // Constructeur avec paramètres
+        public Joueur(string nom, int score, int nbMotsTrouves)
+        {
+
+        }
+
+        // Constructeur par clônage
+        public Joueur(Joueur g)
+        {
+
+        }
+
+        #endregion
+
+
+        //----[ Propriétés ]----//
+        #region Propriétés
+
+        #endregion
+
+
+        //-----[ Méthodes ]-----//
+        #region Méthodes
+
+        #endregion
     }
 }
