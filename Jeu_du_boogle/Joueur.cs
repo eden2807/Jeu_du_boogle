49e05319df56fb996cefbb472a13f1ee1b5dc993
eden <elfden.sup@yahoo.com>
2024-11-09 21:55:15 +0100

    Ajout de la classe "Joueur"

---
+++ Jeu_du_boogle/Joueur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_du_boogle
+{
+    public class Joueur
+    {
+        public string Nom;
+    }
+}
