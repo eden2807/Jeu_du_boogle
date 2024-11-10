149813038e1be3192d1d0f6963f9ed5de7c02ea4
eden <elfden.sup@yahoo.com>
2024-11-10 09:17:28 +0100

    d

--- Jeu_du_boogle/Joueur.cs
+++ Jeu_du_boogle/Joueur.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Jeu_du_boogle
+﻿namespace Jeu_du_boogle
 {
     public class Joueur
     {
