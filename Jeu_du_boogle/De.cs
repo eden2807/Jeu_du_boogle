4a5cc08b59b90f709c6c94f0a056ef01dfbadaad
eden <elfden.sup@yahoo.com>
2024-11-11 19:46:49 +0100

    f

--- Jeu_du_boogle/De.cs
+++ Jeu_du_boogle/De.cs
@@ -8,13 +8,58 @@ namespace Jeu_du_boogle
 {
     public class De
     {
-        // test dé
-        // ghktdfy
 
-        // modif dé test e
-        private int a = 0;
-        // a représente un entier
+        //-----[ Attributs ]-----//
+        #region Attributs
+        string[] lettres;
+        int face;
 
-        // modif dé test eden 
+        char Lettre_Visible;
+        #endregion
+
+        //---[ Constructeurs ]---//
+        #region Constructeurs
+        public De(string[] lettres) 
+        {
+            if (lettres.Length != 6)
+                throw new ArgumentException("Le dé doit avoir 6 lettres exactement");
+            //si la taille de lettres est différente de 6, on sort.
+            
+
+            for(int i = 0; i < lettres.Length; i++)
+            {
+                char dumpchar = lettres[0][i];
+                for(int j = 0; j < lettres[i].Length; j++)
+                {
+                    if (lettres[i][j] == dumpchar) // on vérifie que chaque caractère est unique à la main.
+                    {
+                        throw new ArgumentException("les lettres ne sont pas uniques !"); 
+                    }
+                }
+            }
+
+            this.lettres = lettres;
+            this.face = 0;
+        }
+        #endregion
+
+        //----[ Propriétés ]----//
+        #region Propriétés
+
+        public string[] Lettres
+        {
+            get { return this.lettres; }
+        }
+
+
+
+        #endregion
+
+
+        //-----[ Méthodes ]-----//
+        #region Méthodes
+
+        #endregion
     }
+
 }
