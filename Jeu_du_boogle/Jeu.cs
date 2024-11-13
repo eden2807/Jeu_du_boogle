dcf160662530fbc15ee919dc9ff8ab8a3cc45f8e
Ryan Fihr HULTON <ryan-fihr.hulton@edu.devinci.fr>
2024-11-13 21:29:51 +0100

    Première config du Main terminée

--- Jeu_du_boogle/Jeu.cs
+++ Jeu_du_boogle/Jeu.cs
@@ -6,11 +6,14 @@ using System.Windows.Forms;
 
 namespace Jeu_du_boogle
 {
-    public static class Jeu
+    internal class Jeu
     {
         public static void Main(string[] args)
         {
-            // Configuration initiale //
+//--------------------[ Configuration initiale ]--------------------//
+
+            // Choix de la langue //
+            #region Langue
 
             Console.WriteLine("---[ BIENVENUE AU JEU DU BOOGLE ! ]---\n");
 
@@ -18,17 +21,37 @@ namespace Jeu_du_boogle
             Console.WriteLine("\t Français : F");
             Console.WriteLine("\t English : E");
 
-            char langue;
+            string langue;
             do
             {
-                Console.Write("--->");
-                langue = Convert.ToChar(Console.ReadLine());
-                if (langue != 'E' || langue != 'F') Console.WriteLine("Le caractère saisi est invalide. Veuillez recommencer.");
+                Console.Write("---> ");
+                langue = Console.ReadLine();
+                if (langue != "E" && langue != "F") Console.WriteLine("Le caractère saisi est invalide. Veuillez recommencer.");
             }
-            while (langue != 'E' || langue != 'F');
+            while (langue != "E" && langue != "F");
+            Console.WriteLine();
 
-            if (langue == 'F') Console.WriteLine("---[ Vous avez sélectionné FRANCAIS ]---");
-            if (langue == 'E') Console.WriteLine("---[ You have selected ENGLISH ]---");
+            if (langue == "F") Console.WriteLine("---[ Vous avez sélectionné FRANCAIS ]---\n");
+            if (langue == "E") Console.WriteLine("---[ You have selected ENGLISH ]---\n");
+
+            // Il manquera la configuration des fichiers dictionnaire (et lettres ?) en fonction de la langue
+
+            #endregion
+
+            // Taille du plateau //
+            #region Plateau
+
+            #endregion
+
+            // Pseudos des joueurs //
+            #region Pseudos
+
+            #endregion
+
+
+//--------------------[ Les parties du jeu ]--------------------//
+
+
         }
     }
 }
